45d8f89c11ce9fe8d1fe4dfafc3bfaae00feb579
Paul Miller <paul@voltar.org>
2015-04-14 14:42:25 -0400

    ok, I think this all works.  It could use some tweaks though

--- python/vector_selector.py
+++ python/vector_selector.py
@@ -3,8 +3,8 @@ import numpy, math
 from gnuradio import gr
 from operator import itemgetter
 
-INDEX_MODE  = 0
-MINMAX_MODE = 1
+INDEX_MODE  = 1234
+MINMAX_MODE = 5678
 
 class vector_selector(gr.sync_block):
     """
@@ -59,10 +59,11 @@ class vector_selector(gr.sync_block):
         self._indices  = indices
         self._debug    = debug
         self._modality = modality
+        self._dtype    = dtype
 
         gr.sync_block.__init__(self, "vector_selector",
             [ ", ".join( [dtype] * vec_len ) ],
-            [dtype] * self._outputs
+            [ "%s,%s" % (dtype,dtype) ] * self._outputs if modality == MINMAX_MODE else [ dtype ] * self._outputs
         )
 
     def set_indices(self, indices):
@@ -78,35 +79,37 @@ class vector_selector(gr.sync_block):
                     output_items[ o_stream_idx ][ i_idx ] = c
 
                     if self._debug:
-                        print "spam: <%s> %7.2fi + %7.2fj -> v-slice %d @[%d]" % (
-                            numpy.dtype(c),
-                            c.real, c.imag,
-                            o_stream_idx,
-                            i_idx,
-                        )
+                        if self._dtype == "complex64":
+                            print "spam[idx]: %7.2fi + %7.2fj @[%d] -> @[%d][%d]" % (
+                                c.real, c.imag, i_vec_idx,
+                                o_stream_idx, i_idx,
+                            )
+
+                        else:
+                            print "spam[idx]: %s @[%d] -> @[%d][%d]" % ( c, o_stream_idx, i_idx )
 
         elif self._modality == MINMAX_MODE:
             for (i_idx, i_vec) in enumerate(_in):
                 mlist = []
                 for (i,c) in enumerate(i_vec):
-                    mlist.append( (i, math.sqrt( c.real ** 2 + c.imag ** 2 )) )
-                mlist = sorted(mlist, key=itemgetter(1))
+                    m = math.sqrt( c.real ** 2 + c.imag ** 2 ) if self._dtype == "complex64" else c
+                    mlist.append( (i, c, m) )
+                mlist = sorted(mlist, key=itemgetter(2), reverse=True)
 
                 for (o_stream_idx, i_nth_max) in enumerate(self._indices):
-                  ct = mlist[ i_vec_idx ]
+                  ct = mlist[ i_nth_max ]
+                  ct = (ct[0], ct[1])
                   output_items[ o_stream_idx ][ i_idx ] = ct
 
                   if self._debug:
-                      print "spam: <%s> %7.2fi + %7.2fj @[%d] -> v-slice %d @[%d]" % (
-                          numpy.dtype(ct[1]),
-                          ct[1].real, ct[1].imag, ct[0],
-                          o_stream_idx,
-                          i_idx,
+                    if self._dtype == "complex64":
+                      print "spam[max]: (@[%4d], %7.2fi + %7.2fj) @[%d] -> @[%d][%d]" % (
+                          ct[0], ct[1].real, ct[1].imag, i_nth_max,
+                          o_stream_idx, i_idx,
                       )
+                    else:
+                        print "spam[max]: (@[%4d],%s) -> @[%d][%d]" % ( ct, o_stream_idx, i_idx )
         else:
             raise AttributeError("modality not understood")
-
-        if  self._debug:
-            print "spam: len=%d\n" % len(_in)
 
         return len(_in)
